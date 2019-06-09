e36a2bb06859ceebef1e1d52a629601d42023c08
LeAnh0705 <ldanh26@gmail.com>
2019-06-09 22:52:30 +0700

    Format code and Css

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -7,21 +7,20 @@ import {HeaderComponent} from './component/header/header.component';
 import {FooterComponent} from './component/footer/footer.component';
 import {UserloginComponent} from './component/userlogin/userlogin.component';
 import {MainpageComponent} from './component/mainpage/mainpage.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import {MDBBootstrapModule } from 'angular-bootstrap-md'
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
-
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {HttpErrorInterceptor} from "./http-error.interceptor";
-import * as Rollbar from "rollbar";
-import {StoreModule} from "@ngrx/store";
-import {reducers} from "./store/reducers";
-import {EffectsModule} from "@ngrx/effects";
-import {effects} from "./store/effects";
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpErrorInterceptor} from './http-error.interceptor';
+import * as Rollbar from 'rollbar';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
 import { BookComponent } from './component/book/book.component';
-import {JwtHelperService} from "@auth0/angular-jwt";
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {RootStoreModule} from './root_store';
 
 const rollbarConfig = {
   captureUncaught: true,
@@ -52,14 +51,9 @@ export const RollbarService = new InjectionToken<Rollbar>('rollbar');
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    StoreModule.forRoot(
-      {
-        user: reducers.user
-      }
-    ),
-    EffectsModule.forRoot(effects),
     HttpClientModule,
-
+    RootStoreModule,
+    ScrollingModule
   ],
   providers: [
     {
